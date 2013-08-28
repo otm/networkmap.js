0f3c5da4f8566cdc88b688eae048b582a4a65ed0
Nils Lagerkvist <nils.lagerkvist@tele2.com>
2013-08-28 13:51:06 +0000

    Redesigned hover events on links.
    It is now easy to inject custom HTML on the hover event.
    The hover event function looks like this now function(e, link, el)
     e - event
     link - the link which the mouse is over
     el - a div to inject HTML in
    
    By default this function will display the link name

--- dist/networkmap.js
+++ dist/networkmap.js
@@ -309,79 +309,8 @@ networkMap.registerDatasource('simulate', function(url, requests){
 			window.location.href = link.options.events.click.href;
 		}
 	},
-	hover: function(e, link){
-		var el = document.id('nm-active-hover');
-		var id = e.target.instance.attr('id');
-		
-		if (el){
-			if (el && el.retrieve('id') !== e.target.instance.attr('id')){
-				el.destroy();	
-			}
-			else{
-				el.store('keep', true);
-				return;
-			}	
-		}
-		
-		
-		var position = e.target.getPosition(),
-			svg = e.target.instance;
-			
-			
-		var segment11 = svg.getSegment(2),
-			segment12 = svg.getSegment(3),
-			segment21 = svg.getSegment(5),
-			segment22 = svg.getSegment(6);
-		
-		var midX = ((segment11.coords[0] + segment22.coords[0])/2 +
-			(segment12.coords[0] + segment21.coords[0])/2)/2;
-
-		var midY = ((segment11.coords[1] + segment22.coords[1])/2 +
-			(segment12.coords[1] + segment21.coords[1])/2)/2;
-
-		el = new Element('div', {
-			'id': 'nm-active-hover',
-			'class': 'nm-hover',
-			'text': link.options.name,
-			events: {
-				mouseover: function(){
-					el.store('mouseover', true);
-				},
-				mouseout: function(){
-					el.eliminate('mouseover');
-					(function(){
-						if (!el.retrieve('keep'))
-							el.destroy();
-						else
-							el.eliminate('keep');
-					}).delay(10);
-				},
-				click: function(ev){
-					// swap targets :)
-					//ev.target = e.target;
-					//e.target.instance.clickHandler(e);
-					link._clickHandler(e);
-				}
-					
-				
-			}
-		})
-		.store('id', e.target.instance.attr('id'));
-		
-		el.setStyles({
-			top: -1000,
-			left: -1000	
-		});
-				
-		
-		document.id(document.body).grab(el);
-		var size = el.getSize();
-		el.setStyles({
-			top: midY - size.y/2 + e.target.instance.doc().parent.getPosition().y,
-			left: midX - size.x/2 + e.target.instance.doc().parent.getPosition().x
-		});
-		
-
+	hover: function(e, link, el){
+		el.set('text', link.options.name);
 	},
 	mouseover: function(e, options, hover){
 		console.log("mouse over");
@@ -403,7 +332,78 @@ networkMap.registerEvent = function(name, f){
 	}
 	else if (name === 'hover'){	
 		networkMap.events.mouseover = function(e, link){
-			f(e, link);
+			var el = document.id('nm-active-hover');
+			var id = e.target.instance.attr('id');
+			
+			if (el){
+				if (el && el.retrieve('id') !== e.target.instance.attr('id')){
+					el.destroy();	
+				}
+				else{
+					el.store('keep', true);
+					return;
+				}	
+			}
+			
+			
+			var position = e.target.getPosition(),
+				svg = e.target.instance;
+				
+				
+			var segment11 = svg.getSegment(2),
+				segment12 = svg.getSegment(3),
+				segment21 = svg.getSegment(5),
+				segment22 = svg.getSegment(6);
+			
+			var midX = ((segment11.coords[0] + segment22.coords[0])/2 +
+				(segment12.coords[0] + segment21.coords[0])/2)/2;
+	
+			var midY = ((segment11.coords[1] + segment22.coords[1])/2 +
+				(segment12.coords[1] + segment21.coords[1])/2)/2;
+	
+			el = new Element('div', {
+				'id': 'nm-active-hover',
+				'class': 'nm-hover',
+				events: {
+					mouseover: function(){
+						el.store('mouseover', true);
+					},
+					mouseout: function(){
+						el.eliminate('mouseover');
+						(function(){
+							if (!el.retrieve('keep'))
+								el.destroy();
+							else
+								el.eliminate('keep');
+						}).delay(10);
+					},
+					click: function(ev){
+						// swap targets :)
+						//ev.target = e.target;
+						//e.target.instance.clickHandler(e);
+						link._clickHandler(e);
+					}
+						
+					
+				}
+			})
+			.store('id', e.target.instance.attr('id'));
+			
+			el.setStyles({
+				top: -1000,
+				left: -1000	
+			});
+					
+			
+			document.id(document.body).grab(el);
+						
+			f(e, link, el);
+			
+			var size = el.getSize();
+			el.setStyles({
+				top: midY - size.y/2 + e.target.instance.doc().parent.getPosition().y,
+				left: midX - size.x/2 + e.target.instance.doc().parent.getPosition().x
+			});
 		};
 		
 		networkMap.events.mouseout = function(e, link){
